021cb83be48e64160ebf87eb48f5cede01ab9683
Julius Ojakaa <ojayallday@gmail.com>
2023-07-18 05:54:05 +0300

    registration package build up

---
+++ demo/src/main/java/registration/RegistrationRequest.java
@@ -0,0 +1,9 @@
+package registration;
+
+public class RegistrationRequest {
+    private final String firstName;
+    private final String firstName;
+    private final String firstName;
+    private final String firstName;
+    private final String firstName;
+}
