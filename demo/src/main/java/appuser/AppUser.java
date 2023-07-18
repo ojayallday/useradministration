60a1b14e1a3dc5d63f06f67bda65e913fd9699cf
Julius Ojakaa <ojayallday@gmail.com>
2023-07-18 04:41:31 +0300

    Added appuserrole,studentrepository and appuserservice classes

--- demo/src/main/java/appuser/AppUser.java
+++ demo/src/main/java/appuser/AppUser.java
@@ -1,5 +1,6 @@
 package appuser;
 
+import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,16 +16,46 @@ import java.util.Collections;
 @Setter
 @EqualsAndHashCode
 @NoArgsConstructor
+@Entity
+
 
 public class AppUser implements UserDetails {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String username;
     private String  email;
     private String password;
+    @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
+
+    public AppUser(String name,
+                   String username,
+                   String email,
+                   String password,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         SimpleGrantedAuthority authority=
---
+++ demo/src/main/java/appuser/StudentRepository.java
@@ -0,0 +1,4 @@
+package appuser;
+
+public class StudentRepository {
+}
